dc8ea31c7c42bc5f0f2eea50c6a3275cae08236f
sopy <gggameforevergg57@gmail.com>
2026-09-01 04:04:45 +0300

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 export default {
-  schema: "./src/database/schema.js",
+  schema: "schema.js",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: { url: process.env.DATABASE_URL }
