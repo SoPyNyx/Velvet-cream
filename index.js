a2df38d41b0ecc5b6de79fa6e13a298b594d6fd7
sopy <gggameforevergg57@gmail.com>
2026-09-01 04:11:46 +0300

    Refactor import path for client module

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { config } from "./config.js";
-import { client } from "./bot/client.js";
+import { client } from "./client.js";
 if(!config.token) throw new Error("DISCORD_TOKEN is required");
 await client.login(config.token);
