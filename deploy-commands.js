b41f92393bf1a91e304be540f2bd2614d6fed024
sopy <gggameforevergg57@gmail.com>
2026-09-01 04:26:59 +0300

    Fix deploy-commands.js import path - commands.js is at root level

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { REST, Routes } from "discord.js";
-import { commands } from "./bot/commands.js";
+import { commands } from "./commands.js";
 const rest=new REST({version:"10"}).setToken(process.env.DISCORD_TOKEN);
 await rest.put(Routes.applicationCommands(process.env.CLIENT_ID),{body:commands.map(c=>c.toJSON())});
 console.log(`Registered ${commands.length} commands.`);
